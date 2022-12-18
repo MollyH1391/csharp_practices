9c54d7eac2bb76a1d60258579cc49bbe334d1e01
molly huang <molly.huang@wingwill.com.tw>
2022-12-18 21:45:33 +0800

    edit activitiescontroller and get http response

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,3 +1,6 @@
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace API.Controllers
@@ -7,7 +10,20 @@ namespace API.Controllers
         private readonly DataContext _context;
         public ActivitiesController(DataContext context)
         {
-            this._context = context;
+            _context = context;
         }
+
+        [HttpGet] //api/activities
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")] //api/activities/id
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+        }
+
     }
 }
