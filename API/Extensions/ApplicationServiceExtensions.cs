2449d523fc0aca28a6b1730b609f33cee13cd6a4
molly huang <molly.huang@wingwill.com.tw>
2023-01-20 23:46:14 +0800

    edit

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -8,7 +8,7 @@ namespace API.Extensions
 {
     public static class ApplicationServiceExtensions
     {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        public static IServiceCollection AddAplicationServices(this IServiceCollection services,
             IConfiguration config)
         {
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- API/Program.cs
+++ API/Program.cs
@@ -7,7 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddAplicationServices(builder.Configuration);
+
 
 var app = builder.Build();
 
